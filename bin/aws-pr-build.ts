41d0a3a50c42113c2594dd4917819748591f2e4c
Khanh Tran <ktran@cas.org>
2022-05-03 17:31:26 -0400

    pr

--- bin/aws-pr-build.ts
+++ bin/aws-pr-build.ts
@@ -4,5 +4,13 @@ import * as cdk from '@aws-cdk/core';
 import { AwsPrBuildStack } from '../lib/aws-pr-build-stack';
 
 const app = new cdk.App();
-new AwsPrBuildStack(app, 'AwsPrBuildStack', {
-});
+const props = {
+    env: {
+        account: '657641750194',
+        region: 'us-east-2'
+    }
+}
+new AwsPrBuildStack(app, 'AwsPrBuildStack', props);
+
+
+app.synth()
--- lib/aws-pr-build-stack.ts
+++ lib/aws-pr-build-stack.ts
@@ -1,22 +1,69 @@
-import * as cdk from '@aws-cdk/core';
+import { App, Stack, Construct, StackProps } from "@aws-cdk/core";
+import { Repository } from "@aws-cdk/aws-codecommit";
+import { ServicePrincipal, Effect, ManagedPolicy, PolicyDocument, PolicyStatement, Role } from "@aws-cdk/aws-iam";
+import { Repository as EcrRepository } from "@aws-cdk/aws-ecr";
+import { IProject, Project, Source, BuildSpec, LinuxBuildImage } from "@aws-cdk/aws-codebuild"
+import { RuleTargetInput, EventField } from "@aws-cdk/aws-events";
 import * as events from '@aws-cdk/aws-events'
 import * as targets from '@aws-cdk/aws-events-targets'
 
-export class AwsPrBuildStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
+export class AwsPrBuildStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
+
     new events.Rule(this, `pr-create`, {
-            eventPattern: {
-                source: ['aws.codecommit'],
-                detailType: ['CodeCommit Pull Request State Change'],
-                resources: ['arn:aws:codecommit:us-east-2:657641750194:sample'],
-                detail: {
-                    event: ['pullRequestCreated', 'pullRequestSourceBranchUpdated'],
-                    pullRequestStatus: "Open"
-                },
-            },
-            //targets: [new targets.CodeBuildProject(this)]
+      eventPattern: {
+        source: ['aws.codecommit'],
+        detailType: ['CodeCommit Pull Request State Change'],
+        resources: ['arn:aws:codecommit:us-east-2:657641750194:sample'],
+        detail: {
+          event: ['pullRequestCreated', 'pullRequestSourceBranchUpdated'],
+          pullRequestStatus: "Open"
+        },
+      },
+      targets: [new targets.CodeBuildProject(this.newProject(), {
+        event: events.RuleTargetInput.fromObject({
+          sourceVersion: EventField.fromPath('$.detail.sourceReference'),
+          environmentVariablesOverride: [{
+            'name': 'BRANCH_NAME',
+            'value': EventField.fromPath('$.detail.sourceReference'),
+            'type': 'PLAINTEXT'
+          }]
         })
+      })]
+    })
+
+
   }
+
+  private newProject(): IProject {
+    const repo = Repository.fromRepositoryName(
+      this,
+      "repo",
+      "sample-repo"
+    );
+
+    return new Project(this, "pull-request-build", {
+      source: Source.codeCommit({ repository: repo }),
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_5_0
+      },
+      buildSpec: BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'ls -la'
+            ]
+          }
+        }
+      }),
+      role: new Role(this, "pull-request-build-role", {
+        assumedBy: new ServicePrincipal('codebuild.amazonaws.com')
+      })
+    })
+  }
+
+
 }
