097d1ae902cc87afe41d29cd5021e9507c60a722
Khanh Tran <trancongkhanh@gmail.com>
2022-06-30 16:55:05 -0400

    Update aws-pr-build.ts

--- bin/aws-pr-build.ts
+++ bin/aws-pr-build.ts
@@ -6,7 +6,7 @@ import { AwsPrBuildStack } from '../lib/aws-pr-build-stack';
 const app = new cdk.App();
 const props = {
     env: {
-        account: '657641750194',
+        account: '',
         region: 'us-east-2'
     }
 }
